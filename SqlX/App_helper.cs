619ddcd82f5934936a325e54ea24be0c375edeb5
Frederik-Mainbranch <frederik.hartung@gmx.de>
2022-04-14 15:55:37 +0200

    Added Sql_MS_helper and implemented basic CRUD operations

---
+++ SqlX/App_helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helperklassen
+{
+    public static class App_helper
+    {
+        public static void Nutze_MsSql(string _conectionstring)
+        {
+            Sql_MS_helper.Connectionstring = _conectionstring;
+        }
+    }
+}
---
+++ SqlX/Log_helper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helperklassen
+{
+    public static class Log_helper
+    {
+        public static void Create_errorMessage(string errorMessage)
+        {
+
+        }
+
+        public static void Create_log(string logMessage)
+        {
+
+        }
+    }
+}
